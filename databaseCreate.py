31e5afa7e78c7f5a9b6d86412de8d222c755efd1
Matthew Langner <mattlangs@gmail.com>
2017-04-04 21:31:40 +0100

    Changed the loop

--- databaseCreate.py
+++ databaseCreate.py
@@ -4,15 +4,16 @@ import sqlite3 # imports to sqlite3 module
 
 def createFile ():
 	''''To create a new file'''
+	sqliteFile = ''
+	sqliteFile = str(input('\nPlease enter your new file name: ')).strip()
+	sqliteFileLenght = len(sqliteFile)
 	while True:
-		sqliteFile = ''
-		sqliteFile = str(input('\nPlease enter your new file name: ')).strip()
-		sqliteFileLenght = len(sqliteFile)
 		if sqliteFileLenght == 0:
 			sqliteFile = str(input('\nNo filename entered, please try again.\nPlease enter your new file name: ')).strip()
-			sqliteFileLenght = len(sqlite_file_enter)
+			sqliteFileLenght = len(sqliteFile)
 		elif sqliteFileLenght > 0:
 			return sqliteFile
 		else:
 			print('Error code 02')
+			sys.exit('Error code 02')
 			return 
