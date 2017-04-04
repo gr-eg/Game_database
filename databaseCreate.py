24c9a9761a57e0f045736f8aad25a36f44339ed5
Matthew Langner <mattlangs@gmail.com>
2017-04-04 21:14:29 +0100

    Creating loop for database name entry
    added an if statement and while True loop.

--- databaseCreate.py
+++ databaseCreate.py
@@ -4,9 +4,15 @@ import sqlite3 # imports to sqlite3 module
 
 def createFile ():
 	''''To create a new file'''
-	sqliteFile = ''
-	sqliteFile = str(input('\nPlease enter your file name: ')).strip()
-	sqliteFileLenght = len(sqliteFile)
-	while sqliteFileLenght == 0:
-		sqliteFile = str(input('\nNo filename entered, please try again.\nPlease enter your file name: ')).strip()
-		sqliteFileLenght = len(sqlite_file_enter)
+	while True:
+		sqliteFile = ''
+		sqliteFile = str(input('\nPlease enter your new file name: ')).strip()
+		sqliteFileLenght = len(sqliteFile)
+		if sqliteFileLenght == 0:
+			sqliteFile = str(input('\nNo filename entered, please try again.\nPlease enter your new file name: ')).strip()
+			sqliteFileLenght = len(sqlite_file_enter)
+		elif sqliteFileLenght > 0:
+			return sqliteFile
+		else:
+			print('Error code 02')
+			return 
