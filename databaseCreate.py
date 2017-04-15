b240722767d319970324a00576dbbd545969c919
Matthew Langner <mattlangs@gmail.com>
2017-04-15 22:06:01 +0100

    Adding  to the create file
    adding a version control

--- databaseCreate.py
+++ databaseCreate.py
@@ -1,5 +1,7 @@
 #The creation of the game database
 import sqlite3
+import databaseVersion
+
 
 def createDatabase(sqliteFile):
 	'''The creation of the database'''
@@ -10,6 +12,12 @@ def createDatabase(sqliteFile):
 		
 		# c.executescript to create lots of tables at once
 		c.execute('''CREATE TABLE games (id_game_main INTEGER PRIMARY KEY, title TEXT, description TEXT)''')
+		
+		#database version identifier 
+		c.execute('''CREATE TABLE databaseVersion (version INTEGER PRIMARY KEY)''')
+		
+		#add preset values
+		c.execute('INSERT INTO databaseVersion VALUES(?)', databaseVersion.version)
 		
 		return 
 		
---
+++ databaseVersion.py
@@ -0,0 +1,7 @@
+#State what version of the database is been used
+
+def version ():
+	'''Stating what version of the database is been used - this is static per build.'''
+	version = 1
+	
+	return version
